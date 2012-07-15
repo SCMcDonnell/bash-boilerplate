ecb8227d80bb74613afe16df1a2520f2655654dd
Adam Matan <adam@matan.name>
2012-07-15 15:07:52 +0300

    Fixed tests.

--- tests/test2.sh
+++ tests/test2.sh
@@ -3,7 +3,7 @@
 source ../boilerplate.sh
 
 exit_on_error 'echo "1+2" | bc'
-exit_on_error 'asdasdsa asa sgfh jyh'
+exit_on_error 'asdasdsa asa sgfh jyh' 2>&1
 
 echo "You should never see this line"
 
